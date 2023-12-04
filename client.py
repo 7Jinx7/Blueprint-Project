95ffb7a6f7ba27d67b704800f71157913d055aa0
dgmangibin <dgm95@rutgers.edu>
2023-12-04 18:58:35 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+from getpass import getpass
 import threading
 import socket
 
@@ -12,19 +13,29 @@ except socket.error as err:
 server_binding = ("localhost", 9999)
 cs.connect(server_binding)
 
-# recieve data from server: Welcome to Blueprint  
+# recieve data from server
+
+#username
+username = input("Enter username: ")
+cs.send(username.encode())
+
+#Message received confirmation 
 data_from_server=cs.recv(1024)
 message = data_from_server.decode()
-print("[C]: Data received from server: " + message)    
-cs.send(input("Response here: ").encode())
+print("[C]: Data received from server: " + message)
 
-# IN GROUPS
+
+#password 
+password = getpass.getpass("Enter password: ")
+cs.send(password.encode())
 
 # receive data from server: How are you
 data_from_server=cs.recv(1024)
 message = data_from_server.decode()
 print("[C]: Data received from server: " + message)    
 cs.send(input("Response here: ").encode())
+
+
 
 count = 0
 while(count < 5): # use loop to send 1, 2, 3, 4, 5 to client
@@ -34,8 +45,7 @@ while(count < 5): # use loop to send 1, 2, 3, 4, 5 to client
     cs.send(response.encode())
     count+=1
 
-    
-    
+     
 # close the client sockets
 cs.close()
 exit()
